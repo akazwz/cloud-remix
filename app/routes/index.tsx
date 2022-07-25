c793eb7d10509995c6c87e5948c4bdb77064dfb4
akazwz <akazwz@pm.me>
2022-07-25 10:59:04 +0800

    add chakra ui and fix emotion to work with remix and cloudflare

---
+++ app/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+const Index = () => {
+	return (
+		<Box>
+			<Heading>Index</Heading>
+		</Box>
+	)
+}
+
+export default Index
--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,36 @@
-/** @type {import('@remix-run/dev').AppConfig} */
+const { withEsbuildOverride } = require('remix-esbuild-override')
+const path = require('node:path')
+const GlobalsPolyfills =
+	require('@esbuild-plugins/node-globals-polyfill').default
+
+withEsbuildOverride((option, { isServer }) => {
+	option.jsxFactory = 'jsx'
+	// eslint-disable-next-line no-undef
+	option.inject = [path.resolve(__dirname, 'reactShims.ts')]
+
+	// 🔽 This block is for Cloudflare Workers/Pages. 🔽
+	if (isServer)
+		option.plugins = [
+			GlobalsPolyfills({
+				buffer: true,
+			}),
+			...option.plugins,
+		]
+	// 🔼 This block is for Cloudflare Workers/Pages. 🔼
+
+	return option
+})
+
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
 module.exports = {
-  serverBuildTarget: "cloudflare-workers",
-  server: "./server.js",
-  devServerBroadcastDelay: 1000,
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-};
+	serverBuildTarget: 'cloudflare-workers',
+	server: './server.js',
+	devServerBroadcastDelay: 1000,
+	ignoredRouteFiles: ['.*'],
+	// appDirectory: "app",
+	// assetsBuildDirectory: "public/build",
+	// serverBuildPath: "build/index.js",
+	// publicPath: "/build/",
+}
