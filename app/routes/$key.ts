b7956ea867b8a0bb6549ccc6734b76995da2d870
akazwz <akazwz@pm.me>
2022-07-30 10:57:06 +0800

    update key route and direct get object by key

---
+++ app/routes/$key.ts
@@ -0,0 +1,44 @@
+import type { LoaderFunction } from '@remix-run/cloudflare'
+import { json } from '@remix-run/cloudflare'
+import mime from 'mime-types'
+
+// get object by key
+// 通过 key 获取 对象
+export const loader: LoaderFunction = async({ request, params }) => {
+	const { key } = params
+	// 参数错误
+	if (!key) {
+		return json(
+			{ msg: 'no key' },
+			{ status: 400 },
+		)
+	}
+
+	const url = new URL(request.url)
+	// key.ext?d=1
+	const download = url.searchParams.get('d')
+
+	let mimeType = 'application/octet-stream'
+	if (!download) {
+		mimeType = mime.lookup(key) || 'application/octet-stream'
+	}
+
+	const object = await MY_BUCKET.get(key)
+	if (!object) {
+		return json(
+			{ msg: 'object no found' },
+			{ status: 404 },
+		)
+	}
+
+	const headers = new Headers()
+	headers.set('Content-Type', mimeType)
+	headers.set('etag', object.httpEtag)
+	return new Response(object.body, {
+		status: 200,
+		headers,
+	})
+}
+
+
+
